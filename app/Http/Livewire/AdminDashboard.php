07921d0ed6cc464e7c22de1b06e969d14b32e438
Shiban Ashiq <shibanashiq743@gmail.com>
2022-01-28 18:03:43 +0000

    Second update

--- app/Http/Livewire/AdminDashboard.php
+++ app/Http/Livewire/AdminDashboard.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Category;
+use App\Models\Product;
 use Livewire\Component;
 
 class AdminDashboard extends Component
@@ -22,7 +23,7 @@ class AdminDashboard extends Component
     public function render()
     {
         $total_category = Category::all()->count();
-        $total_product = Category::all()->count();
+        $total_product = Product::all()->count();
         return view('livewire.admin-dashboard',[
             'total_cate' => $total_category,
             'total_prod' => $total_product,
